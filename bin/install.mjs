c571c42345cdfc840425079e06df5153c9d790f2
Iuri F <98666631+Nessunskill@users.noreply.github.com>
2023-10-10 12:51:49 +0300

    ...............

--- bin/install.mjs
+++ bin/install.mjs
@@ -4,6 +4,7 @@ import cp from "child_process";
 import path from "path";
 import fs, { existsSync, mkdirSync } from "fs";
 // cli spinners
+import ora from "ora";
 
 // convert libs to promises
 const exec = promisify(cp.exec);
@@ -32,9 +33,12 @@ else {
 }
 
 try {
+    const gitSpinner = ora("Downloading files...").start();
     // clone the repo into the project folder -> creates the new boilerplate
     await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
+    gitSpinner.succeed();
 
+    const cleanSpinner = ora("Removing useless files").start();
     // remove my git history
     const rmGit = rm(path.join(projectPath, ".git"), { recursive: true, force: true });
     // remove the installation file
@@ -43,8 +47,12 @@ try {
 
     process.chdir(projectPath);
     // remove the packages needed for cli
+    await exec("npm uninstall ora cli-spinners");
+    cleanSpinner.succeed();
 
+    const npmSpinner = ora("Installing dependencies...").start();
     await exec("npm install");
+    npmSpinner.succeed();
 
     console.log("The installation is done!");
     console.log("You can now run your app with:");
